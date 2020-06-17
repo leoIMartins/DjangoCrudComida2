fa08c46f9437d7521e84ca572b9579499c5e2793
Leonardo Ignacio Martins <leonardo_ignacio2010@hotmail.com>
2020-06-17 20:39:09 -0300

    projeto completo

--- comida/forms.py
+++ comida/forms.py
@@ -6,3 +6,5 @@ class ComidaForm(forms.ModelForm):
     class Meta:
         model = Comida
         fields = ['tipo', 'descricao', 'quantidade', 'opcoes', 'valorCalorico', 'salada']
+        labels = {'tipo': 'Tipo', 'descricao': 'Descrição', 'quantidade': 'quantidade',
+                  'opcoes': 'Opções', 'valorCalorico': 'Valor Calórico', 'quantidade': 'Quantidade'}
